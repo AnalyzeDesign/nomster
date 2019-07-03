c9a1913fbcd5fffa790cc5e75d139bb3a071f500
Brian K <briankayone@gmail.com>
2019-07-03 02:53:00 +0000

    Lesson 40: Emailing Basics

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,7 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    logger.debug "What is the place?"
-    logger.debug @place
-
     @place.photos.create(photo_params)
-
-    logger.debug "What is the status of photos?"
-    logger.debug @place.photos.inspect
-
     redirect_to place_path(@place)
   end
 
